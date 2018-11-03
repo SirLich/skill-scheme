0b936058bfe58b954661ae1c36f9a3a2768fd9f7
SirLich <lrkoehler77@gmail.com>
2018-11-03 13:06:53 -0500

    Fixed MagmaBow to follow convention

--- src/main/java/plugin/sirlich/skills/passive/MagmaBow.java
+++ src/main/java/plugin/sirlich/skills/passive/MagmaBow.java
@@ -22,23 +22,42 @@ import java.util.Random;
 
 public class MagmaBow extends Skill {
 
+    //This all looks great!
     private static String id = "MagmaBow";
     private static List<Double> chance = getYaml(id).getDoubleList("values.chance");
     private static List<Integer> fireTicks = getYaml(id).getIntegerList("values.fireTicks");
 
+    //This looks good as well
     public MagmaBow(RpgPlayer rpgPlayer, int level){
         super(rpgPlayer,level,"MagmaBow");
     }
 
+    /*
+    The issue here is that you ask about a scoreboard tag which you don't set. If you wanted to power
+    this skill through scoreboard tags, you would have had to set the scoreboard tag. See onArrowFire in PhantomArrows
+    for an example.
+
+    Secondly, we don't actually need to use Scoreboard tags at all (and if we can, we should avoid it).
+
+    Remember: the method onArrowHitEntity you've written bellow will ONLY get called if the player has this skill equiped.
+    That means that as SOON as an arrow hits somebody, you know that its a magma arrow. Since this skill effects ALL arrows,
+    you don't need any checks (beyond just the check for chance).
+     */
     @Override
     public void onArrowHitEntity(ProjectileHitEvent event){
         Entity entity = event.getHitEntity();
-        Player player = (Player) event.getEntity().getShooter();
-        System.out.println(entity.getScoreboardTags().toString());
+        
+        //We can directly cast, since the method we override already does a lot of error checking for us.
+        LivingEntity livingEntity = (LivingEntity) entity;
+
+        //Use of random is good here
         Random rand = new Random();
-        if(entity instanceof LivingEntity && event.getEntity().getScoreboardTags().contains("MAGMA_ARROW") && rand.nextInt() <= chance.get(getLevel())){
-            LivingEntity livingEntity = (LivingEntity) entity;
-            livingEntity.setFireTicks(fireTicks.get(getLevel()));
+
+        //simplified if, based on comments above
+        if(rand.nextInt() <= chance.get(getLevel())){
+
+            //We are ADDING fire ticks, so you have to get fire ticks first, then add the new ones.
+            livingEntity.setFireTicks(livingEntity.getFireTicks() + fireTicks.get(getLevel()));
         }
     }
 
