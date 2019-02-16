4697196d890821e266f81a276d3c97e37f19eac8
SirLich <lrkoehler77@gmail.com>
2019-02-16 12:57:03 -0600

    Add comments to PlayerState ENUM

--- src/main/java/plugin/sirlich/core/PlayerState.java
+++ src/main/java/plugin/sirlich/core/PlayerState.java
@@ -2,5 +2,37 @@ package main.java.plugin.sirlich.core;
 
 public enum PlayerState
 {
-    TESTING, HUB, GAME, LOBBY, SPECTATOR
+    /*
+    This is used for testing the plugin:
+    - Skills are automatically applied
+     */
+    TESTING,
+
+    /*
+    This is for players who are not playing skill-scheme related games at all:
+    - No ability to use skills
+    - No ability to interact with enchantment tables
+     */
+    HUB,
+
+    /*
+    This is for players who are currently inside a skill-scheme game:
+    - Skills can be used, if useSkill is turned on
+    - Skills should be auto-applied when entering this game-mode
+     */
+    GAME,
+
+    /*
+    This is for players who are waiting in a skillScheme lobby:
+    - Can use the skills editor
+    - Skills are "saved", and can be applied when the game starts
+     */
+    LOBBY,
+
+    /*
+    This is a special mode for players who died during a skillScheme game.
+    - No access to skills
+    - Placed into spectator mode on enter. 
+     */
+    SPECTATOR
 }
