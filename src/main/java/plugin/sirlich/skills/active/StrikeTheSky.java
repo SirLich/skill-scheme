2981a87c10cdd80658d8a8a12d95e9febc2803dd
SirLich <lrkoehler77@gmail.com>
2019-04-18 00:46:12 -0500

    Add StrikeTheSky skill

---
+++ src/main/java/plugin/sirlich/skills/active/StrikeTheSky.java
@@ -0,0 +1,39 @@
+package main.java.plugin.sirlich.skills.active;
+
+import main.java.plugin.sirlich.core.RpgPlayer;
+import main.java.plugin.sirlich.skills.meta.CooldownSkill;
+import main.java.plugin.sirlich.utilities.c;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StrikeTheSky extends CooldownSkill
+{
+    private static String id = "StrikeTheEarth";
+    private List<Double> power = getYaml(id).getDoubleList("values.power");
+    private List<Double> range = getYaml(id).getDoubleList("values.range");
+
+    public StrikeTheSky(RpgPlayer rpgPlayer, int level){
+        super(rpgPlayer,level,id);
+    }
+
+    @Override
+    public ArrayList<String> getDescription(int level){
+        ArrayList<String> lorelines = new ArrayList<String>();
+        lorelines.add(c.dgray + "Create a whirlwind to sucks enemies towards you.");
+        return lorelines;
+    }
+
+
+    @Override
+    public void onSwap(PlayerSwapHandItemsEvent event){
+        if(isCooldown()){return;}
+        Double r = range.get(getLevel());
+        for(Entity entity : event.getPlayer().getNearbyEntities(r,r,r)){
+            entity.setVelocity(getRpgPlayer().getPlayer().getLocation().getDirection().setY(0.6).normalize().multiply(-power.get(getLevel())));
+        }
+        refreshCooldown();
+    }
+}
