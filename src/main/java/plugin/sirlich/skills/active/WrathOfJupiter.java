af9efd1c6548fb9da0f79ac7e2c935d2417f4184
SirLich <lrkoehler77@gmail.com>
2019-04-18 10:55:10 -0500

    HolyStrike now works with the new Skills system

--- src/main/java/plugin/sirlich/skills/active/WrathOfJupiter.java
+++ src/main/java/plugin/sirlich/skills/active/WrathOfJupiter.java
@@ -2,13 +2,22 @@ package main.java.plugin.sirlich.skills.active;
 
 import main.java.plugin.sirlich.skills.meta.CooldownSkill;
 import main.java.plugin.sirlich.core.RpgPlayer;
+import main.java.plugin.sirlich.utilities.c;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+
+import java.util.ArrayList;
 
 public class WrathOfJupiter extends CooldownSkill
 {
     public WrathOfJupiter(RpgPlayer rpgPlayer, int level){
         super(rpgPlayer,level,"WrathOfJupiter");
+    }
+
+    public ArrayList<String> getDescription(int level){
+        ArrayList<String> lorelines = new ArrayList<String>();
+        lorelines.add(c.dgray + "Press F to strike your enemies with lighting");
+        return lorelines;
     }
 
     @Override
--- src/main/java/plugin/sirlich/skills/passive/HolyStrike.java
+++ src/main/java/plugin/sirlich/skills/passive/HolyStrike.java
@@ -2,9 +2,13 @@ package main.java.plugin.sirlich.skills.passive;
 
 import main.java.plugin.sirlich.core.RpgPlayer;
 import main.java.plugin.sirlich.skills.meta.Skill;
+import main.java.plugin.sirlich.utilities.c;
+import org.bukkit.Sound;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class HolyStrike extends Skill
 {
@@ -12,6 +16,11 @@ public class HolyStrike extends Skill
     private static List<Double> chance = getYaml(id).getDoubleList("values.chance");
     private static List<Double> knockback = getYaml(id).getDoubleList("values.knockback");
 
+    public ArrayList<String> getDescription(int level){
+        ArrayList<String> lorelines = new ArrayList<String>();
+        lorelines.add(c.dgray + "Sword strikes have a small chance to deal double knockback");
+        return lorelines;
+    }
 
     public HolyStrike(RpgPlayer rpgPlayer, int level){
         super(rpgPlayer,level,"HolyStrike");
@@ -25,13 +34,12 @@ public class HolyStrike extends Skill
         return knockback.get(getLevel());
     }
 
-//    //HolyStrike
-//            if(rpgPlayer.hasSkill(SkillType.HolyStrike) && isSword(player.getInventory().getItemInMainHand())){
-//    HolyStrike skill = (HolyStrike) rpgPlayer.getSkill(SkillType.HolyStrike);
-//    Random rand = new Random();
-//    if(rand.nextInt() <= skill.getChance()){
-//        player.getWorld().playSound(player.getLocation(), Sound.BLOCK_ANVIL_FALL,1,1);
-//        event.getEntity().setVelocity(player.getLocation().getDirection().setY(0).normalize().multiply(skill.getKnockback()));
-//    }
-//}
+    @Override
+    public void onSwordMeleeAttackOther(EntityDamageByEntityEvent event){
+        Random rand = new Random();
+        if(rand.nextInt() <= chance.get(getLevel())){
+            getRpgPlayer().playSound(Sound.BLOCK_ANVIL_FALL);
+            event.getEntity().setVelocity(getRpgPlayer().getPlayer().getLocation().getDirection().setY(0).normalize().multiply(knockback.get(getLevel())));
+        }
+    }
 }
