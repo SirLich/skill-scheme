ed68426c5b057dc08db5d890235c565c430b4913
SirLich <lrkoehler77@gmail.com>
2019-04-24 20:46:26 -0500

    Add description to HolyStrike

--- src/main/java/plugin/sirlich/skills/passive/HolyStrike.java
+++ src/main/java/plugin/sirlich/skills/passive/HolyStrike.java
@@ -2,6 +2,7 @@ package main.java.plugin.sirlich.skills.passive;
 
 import main.java.plugin.sirlich.core.RpgPlayer;
 import main.java.plugin.sirlich.skills.meta.Skill;
+import main.java.plugin.sirlich.utilities.ChatUtilities;
 import main.java.plugin.sirlich.utilities.c;
 import org.bukkit.Sound;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -16,9 +17,15 @@ public class HolyStrike extends Skill
     private static List<Double> chance = getYaml(id).getDoubleList("values.chance");
     private static List<Double> knockback = getYaml(id).getDoubleList("values.knockback");
 
+    @Override
     public ArrayList<String> getDescription(int level){
         ArrayList<String> lorelines = new ArrayList<String>();
-        lorelines.add(c.dgray + "Sword strikes have a small chance to deal double knockback");
+        lorelines.add(c.dgray + "Smack your opponents down with the ");
+        lorelines.add(c.dgray + "righteous sword of good! Gain a passive knockback");
+        lorelines.add(c.dgray + "chance on each sword strike.");
+        lorelines.add("");
+        lorelines.add(c.dgray + "Chance: " + c.green + chance.get(level)*100 + "%" + c.dgray + " chance");
+        lorelines.add(c.dgray + "Knockback: " + c.green + knockback.get(level) + c.dgray + " knockback");
         return lorelines;
     }
 
@@ -38,7 +45,8 @@ public class HolyStrike extends Skill
     public void onSwordMeleeAttackOther(EntityDamageByEntityEvent event){
         Random rand = new Random();
         if(rand.nextInt() <= chance.get(getLevel())){
-            getRpgPlayer().playSound(Sound.BLOCK_ANVIL_FALL);
+            ChatUtilities.useSkill(getRpgPlayer(),this);
+            getRpgPlayer().playSound(Sound.BLOCK_ANVIL_LAND);
             event.getEntity().setVelocity(getRpgPlayer().getPlayer().getLocation().getDirection().setY(0).normalize().multiply(knockback.get(getLevel())));
         }
     }
--- src/main/java/plugin/sirlich/utilities/ChatUtilities.java
+++ src/main/java/plugin/sirlich/utilities/ChatUtilities.java
@@ -1,5 +1,7 @@
 package main.java.plugin.sirlich.utilities;
 
+import main.java.plugin.sirlich.core.RpgPlayer;
+import main.java.plugin.sirlich.skills.meta.Skill;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -7,5 +9,12 @@ public class ChatUtilities
 {
     public void basicChat(Player p, String m){
         p.sendMessage(ChatColor.GRAY + "- " + m);
+    }
+
+    public static void useSkill(RpgPlayer p, Skill s) {
+        useSkill(p.getPlayer(),s);
+    }
+    public static void useSkill(Player p, Skill s){
+        p.sendMessage(c.dgray + ">>>" + c.aqua + s.getName() + c.dgray + " <<<");
     }
 }
