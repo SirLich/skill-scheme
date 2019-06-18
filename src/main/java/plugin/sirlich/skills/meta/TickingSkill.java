8b1cbbb042e2c4442e07993de1269d73516e6c37
SirLich <lrkoehler77@gmail.com>
2019-06-18 17:25:18 -0500

    Add TickingSkill meta-class

---
+++ src/main/java/plugin/sirlich/skills/meta/TickingSkill.java
@@ -0,0 +1,34 @@
+package main.java.plugin.sirlich.skills.meta;
+
+import main.java.plugin.sirlich.SkillScheme;
+import main.java.plugin.sirlich.core.RpgPlayer;
+import org.bukkit.Bukkit;
+
+import java.util.List;
+
+public class TickingSkill extends Skill {
+    private static List<Integer> ticks;
+    private int schedularID;
+
+
+    public TickingSkill(RpgPlayer rpgPlayer, int level, String id){
+        super(rpgPlayer,level,id);
+        ticks = getYaml(id).getIntegerList("values.ticks");
+    }
+
+    public void onTick(){
+
+    }
+
+    public void startTicker(){
+        schedularID = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(SkillScheme.getInstance(), new Runnable() {
+            public void run() {
+                onTick();
+            }
+        }, 0L, ticks.get(getLevel()));
+    }
+
+    public void stopTicker(){
+        Bukkit.getServer().getScheduler().cancelTask(schedularID);
+    }
+}
