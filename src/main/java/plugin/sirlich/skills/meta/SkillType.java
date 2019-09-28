0afa6127b23df0c6e7293af8e8353c7f5a0ee0b1
SirLich <lrkoehler77@gmail.com>
2019-09-28 17:56:23 -0500

    Add Precision Skill

--- src/main/java/plugin/sirlich/skills/meta/SkillType.java
+++ src/main/java/plugin/sirlich/skills/meta/SkillType.java
@@ -33,7 +33,8 @@ public enum SkillType {
     LongShot(new LongShot(null, 0)),
     HuntersThrill(new HuntersThrill(null, 0)),
     Overcharge(new Overcharge(null, 0)),
-    PinDown(new PinDown(null, 0));
+    PinDown(new PinDown(null, 0)),
+    Precision(new Precision(null, 0));
 
     //Overcharge(new Overcharge(null, 0));
 
